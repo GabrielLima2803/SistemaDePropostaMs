f983c13e173fe6b15c879fd03538814840ce1aef
GabrielLima2803 <gabriellima2803@gmail.com>
2025-03-01 22:22:54 -0300

    feat: Adicionando uma query de update

--- proposta-app/src/main/java/com/lima/proposta_app/repository/PropostaRepository.java
+++ proposta-app/src/main/java/com/lima/proposta_app/repository/PropostaRepository.java
@@ -2,12 +2,21 @@ package com.lima.proposta_app.repository;
 
 import java.util.List;
 
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import com.lima.proposta_app.entity.Proposta;
 
+import jakarta.transaction.Transactional;
+
 @Repository
 public interface PropostaRepository extends CrudRepository<Proposta, Long> {
     List<Proposta> findAllByIntegradaFalse();
+
+    @Transactional
+    @Modifying
+    @Query(value = "UPDATE proposta SET aprovada = :aprovada, observacao = :observacao WHERE id = :id", nativeQuery = true)
+    void atualizarProposta(Long id, boolean aprovada, String observacao);
 }
